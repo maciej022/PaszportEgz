fc34d4b1c79abbbde9556bcf190c2bfc89e92ecc
patryksz18 <patrykkarol07@hmail.com>
2025-01-28 14:09:15 +0100

    dziala

--- WpfApp11/MainWindow.xaml.cs
+++ WpfApp11/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Diagnostics.Eventing.Reader;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +20,75 @@ namespace WpfApp11
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void przyciskOk_Click(object sender, RoutedEventArgs e)
+        {
+            string koloroczu;
+            if (kolorNiebieski.IsChecked == true)
+            {
+
+                koloroczu = "Niebieski";
+
+            }
+            else if (kolorPiwne.IsChecked == true)
+            {
+                koloroczu = "Piwne";
+            }
+            else if (kolorZielony.IsChecked == true)
+            {
+                koloroczu = "Zielony";
+            }
+            if (imieText.Text == "" || naziwskoText.Text == "")
+            {
+                MessageBox.Show("Uzupełnij dane");
+            }
+            else
+            {
+                MessageBox.Show($"{imieText.Text} {naziwskoText.Text} {numerText.Text}");
+            }
+
+
+        }
+
+        private void numerText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+            /* if (imieText)
+             {
+                 MessageBox.Show($"{imieText.Text} {numerText.Text} {naziwskoText.Text}")
+             }
+             else
+             {
+                 MessageBox.Show("Wprowadź Dane!");
+             }
+            
+
+            if (kolorNiebieski.IsChecked == true)
+            {
+
+                koloroczu = "Niebieski";
+
+            }
+            else if (kolorPiwne.IsChecked == true)
+            {
+                koloroczu = "Piwne";
+            }
+            else if (kolorZielony.IsChecked == true)
+            {
+                koloroczu = "Zielony";
+            }
+            if (imieText.Text == "" || naziwskoText.Text =="")
+            {
+                MessageBox.Show("Uzupełnij dane");
+            }
+            else
+            {
+                MessageBox.Show($"{imieText.Text} {naziwskoText.Text} {numerText.Text}");
+            }
+
+            */
+          
         }
     }
 }
